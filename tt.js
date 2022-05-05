11965b3bf1e3b4bd94b443b02a05ab4b4b8564c7
King1376 <1185079648@qq.com>
2022-05-05 23:22:31 +0800

    feat: update logo shape

--- tt.js
+++ tt.js
@@ -27,16 +27,16 @@ var src = "";
 //   console.log(src, "src");
 // });
 
-QRCode.toCanvas(dom, "http://www.apple.com", {
+QRCode.toCanvas(dom, "hai哈喽啊123456789987654321就立刻感觉好结果2       56346", {
   errorCorrectionLevel: "L", //容错率L（低）H(高)
-  margin: 2, //二维码内边距，默认为4。单位px
+  margin: 3, //二维码内边距，默认为4。单位px
   height: 200, //二维码高度
   width: 200, //二维码宽度
   scale: 1,
   border: {
-      width: 10,
-      color: "yellowgreen",
-      radius: 15,
+      width: 15,
+      // color: "yellowgreen",
+      radius: 20,
   },
   color: {
     dark: "#7CCD7C", // 二维码背景颜色
@@ -44,9 +44,7 @@ QRCode.toCanvas(dom, "http://www.apple.com", {
   },
   image: {
     logo: logo,
-    dx: 70,
-    dy: 70,
-    dWidth: 60,
-    dHeight: 60,
+    shape: 'roundd',
+    lineWidth: 5
   },
 })
